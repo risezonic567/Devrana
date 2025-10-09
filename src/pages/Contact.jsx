4dfb107cb1cec28e23d279387e74ef984ded0690
Ashish-Kaintur <ashu88793@gmail.com>
2025-10-09 14:06:42 +0530

    chnage in room and detail page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -51,8 +51,8 @@ export default function Contact() {
     return (
         <>
             {/* 🚀 Hero Banner with Animation */}
-            <section className="relative h-[500px] bg-cover bg-center flex items-center justify-center sm:mt-32 mt-[73px] "
-                style={{ backgroundImage: "url('https://i.postimg.cc/MpmmjNWq/contact-us-banner-jpg-02.jpg')" }}>
+            <section className="relative h-[570px] bg-cover bg-center flex items-center justify-center sm:mt-32 mt-[73px] "
+                style={{ backgroundImage: "url('https://i.postimg.cc/kgp6nFb2/contact-us-banner.jpg')" }}>
                 <div className="absolute inset-0 bg-black/50"></div>
                 <motion.div
                     initial={{ opacity: 0, y: 40 }}
